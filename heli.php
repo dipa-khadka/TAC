1535efe2242f94ffe1075d386eca745cdd59c801
dipa-khadka <145959392+dipa-khadka@users.noreply.github.com>
2024-02-21 20:12:41 +0545

    Add files via upload

---
+++ heli.php
@@ -0,0 +1,273 @@
+<?php
+
+include "link.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>under-tour section</title>
+</head>
+<body>
+<!-- top header section -->
+<header>
+    <div class="container-fluid">
+      <nav class="navbar navbar-light py-3 bg-light border-bottom">
+        <div class="container-fluid">
+            <h1><a class="navbar-brand g-4" href="#"> <img src="img/home/logo.webp" alt="" width="90" height="60" class="logo-img ms-5 " style="border-radius: 50%; margin-right: 1rem;"><span class="logo-1 text-danger fw-bold">Tour</span> <span class="logo-1 text-primary fw-bold">& Travels </span> </a>
+            </h1> 
+        
+            <div class="number">
+               <i class="fa-solid fa-phone text-successs fw-bold"> <a href="" class="ms-3 text-success text-decoration-none">+977 9804756981 , 9708876221</a></i> 
+            </div>
+
+          
+
+         <div class="sign">
+        <button type="button" class="btn btn-danger text-center text-light " data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Sign Up
+        </button>
+         </div>
+         </div>
+      </nav>
+    </div>
+  </header>
+
+  <!-- navbar -->
+  <nav class="navbar active navbar-expand-lg navbar-light bg-light p-4 active">
+    <div class="container">
+
+      <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav m-auto  gap-4">
+          <li class="nav-item">
+            <a class="nav-link active fw-bold" href="index.php">Home</a>
+          </li>
+
+          <li class="nav-item"><a href="destination.php" class="nav-link text-dark fw-bold">Destination</a></li>
+          
+          <li class="nav-item"><a href="activities.php" class="nav-link text-dark fw-bold">Activities</a></li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle active fw-bold text-dark" href="activities.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Company Info
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="about.php">About Us </a></li>
+
+              <li><a class="dropdown-item" href="underabout.php">Terms and Conditions</a></li>
+
+              <li><a class="dropdown-item" href="faqs.php">FAQS</a></li>
+            </ul>
+          </li>
+         
+<li class="nav-item"><a href="review.php" class="nav-link text-dark fw-bold">Testimonials</a></li>
+<li class="nav-item"><a href="car-rental.php" class="nav-link text-dark fw-bold">Car-Rental</a></li>
+        <li class="nav-item"><a href="blog.php" class="nav-link text-dark fw-bold">Blog</a></li>
+        <li class="nav-item"><a href="contact.php" class="nav-link text-dark fw-bold">Contact Us</a></li>
+
+      </ul>
+    </div>
+
+    </div>
+  </nav>
+
+    
+
+
+<div class="container-fluid text-center p-5" style="background-color: antiquewhite;">
+    <div class="row">
+        
+        <h2 class="fw-bold text-danger" >Heli Tour</h2>
+        <p class="fw-bold">Enjoy the thrills of paragliding over spectacular landscapes taking unbelievable views of the Himalayan mountains, pristine lakes, and verdant valleys like nowhere else on earth. Nothing can beat the thrill of being up in the sky, flying like a bird in the thermals all the while enjoying views of snow-capped mountains, pristine lakes, and verdant valleys like nowwhere else on earth. With experienced pilots to guid you through the skies, it's an experience of a lifetime. You may fly again someday, but you'll never enjoy the same spectacular views. Choose from tandem flights to fully certified paragliding courses, solo flights, or eveb gi oaragliding accompanied by hawks!</p>    
+</div>
+</div>
+<!-- 2nd part -->
+<div class="container my-4">
+<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow h-md-250 w-75 position-relative">
+        
+<div class="col-auto d-none d-lg-block">
+        <img src="img/car/h1.jpg" class="bd-placeholder-img" width="200" height="250"  alt="">
+
+        </div>
+
+<div class="col p-4 d-flex flex-row position-static">
+          
+<div class="one">
+<h3 class="fw-bold ">Langtang Heli Tour </h3><br>
+         <p class="text-primary fw-bold">Duration : <i class="fa-regular fa-clock text-primary"></i> <span class="text-dark ms-1 text-mute"> 1 Day</span></p>
+</div>
+          
+         <p class="text-light ms-5">gjdkffgjr;dlkhfglhkjsklfjiojio</p>
+          
+         <div class="two">
+            <small>from</small>
+<h3 class="fw-bold text-dark">$ 2,000</h3>
+         <br>
+         <button type="button" class="btn btn-primary text-light">VIEW DETAILS</button>
+         </div>
+        </div>
+        
+      </div>
+</div>
+
+<!-- 2nd section -->
+<div class="container my-4">
+<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow h-md-250 w-75 position-relative">
+        
+<div class="col-auto d-none d-lg-block">
+        <img src="img/car/h2.jpg" class="bd-placeholder-img" width="200" height="250"  alt="">
+
+        </div>
+
+<div class="col p-4 d-flex flex-row position-static">
+          
+<div class="one">
+<h3 class="fw-bold ">Annapurna Heli Tour</h3><br>
+         <p class="text-primary fw-bold">Duration : <i class="fa-regular fa-clock text-primary"></i> <span class="text-dark ms-1 text-mute"> 1 Hour</span></p>
+</div>
+          
+         <p class="text-light ms-5">gjdkffgjr;dlkhfglhkjsklfjiojio</p>
+          
+         <div class="two">
+        all>from</small>
+<h3 class="fw-bold text-dark">$ 250</h3>
+         <br>
+         <button type="button" class="btn btn-primary text-light">VIEW DETAILS</button>
+         </div>
+        </div>
+        
+      </div>
+</div>
+
+<!-- 3rd section -->
+<div class="container  my-4">
+<div class="row g-0 border  rounded overflow-hidden flex-md-row mb-4 shadow h-md-250 w-75 position-relative">
+        
+<div class="col-auto d-none d-lg-block">
+        <img src="img/car/h3.jpg" class="bd-placeholder-img" width="200" height="250"  alt="">
+
+        </div>
+
+<div class="col p-4 d-flex flex-row position-static">
+          
+<div class="one">
+<h3 class="fw-bold ">Everest Helicopter Tour </h3><br>
+         <p class="text-primary fw-bold">Duration : <i class="fa-regular fa-clock text-primary"></i> <span class="text-dark ms-1 text-mute"> 1 Day</span></p>
+</div>
+          
+         <p class="text-light ms-5">gjdkffgjr;dlkhfglhkjsklfjiojio</p>
+          
+         <div class="two">
+            <small>from</small>
+<h3 class="fw-bold text-dark">$ 1,250</h3>
+         <br>
+         <button type="button" class="btn btn-primary text-light">VIEW DETAILS</button>
+         </div>
+        </div>
+        
+      </div>
+</div>
+
+
+<!--partnerships -->
+<div class="container">
+
+<h4 class="ma text-center">WE ARE ASSOCIATED WITH</h4>
+<div class="row align-items-md-stretch">
+  <div class="col-md-6 ">
+    <div class="h-100 p-5 text-white bg-light rounded-3 ">
+      <h6 class="hh text-dark text-center">PAYMENT MODE WE ACCEPT</h6>
+    <img src="img/home/pay.jpg" height="100vh" width="60%" class="rounded mx-auto d-block mt-4" alt="...">
+    </div>
+  </div>
+  <div class="col-md-6 ">
+    <div class="h-100 p-5 text-white bg-light rounded-3 ">
+    <h6 class="hh text-dark text-center">WE ARE AFFILIATED</h6>
+    <img src="img/blog/accept.png" class="rounded mx-auto d-block mt-4" alt="...">
+   
+    </div>
+  </div>
+</div>
+</div>
+
+<!-- footer section -->
+<footer class="footer-section ng-scope mt-4" style="background: rgb(0,0,0,0.1);">
+    <div class="container footer-top">
+      <div class="row">
+        <!-- widget -->
+        <div class="col-sm-6 col-md-3 col-lg-3 footer-widget">
+          <div class="about-widget">
+            <div class="fw-title  mt-4 mb-3" style="margin-top: 10px;">
+              <h3>Our Branches</h3>
+            </div>
+            <div class="dobule-link" style="margin-top: -10px;">
+              <!-- <h3>Our Branches</h3> -->
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"> <i class="fa-solid fa-location-dot text-success ms-4"></i> kathmandu</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-location-dot text-success ms-4"></i>Illam </a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-location-dot text-success ms-4"></i> Pokhara</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-location-dot text-success ms-4"></i>Butwal</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-location-dot text-success ms-4"></i> Nepalganj</a></li>
+
+              </ul>
+            </div>
+
+          </div>
+        </div>
+        <!-- widget -->
+        <div class="col-sm-6 col-md-3 col-lg-3 footer-widget">
+          <div class="fw-title  mt-4 mb-3">
+            <h3>Quick Links</h3>
+          </div>
+          <div class="dobule-link">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Destination</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Home</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Activities</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Trip Types</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Pages</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Style Guid</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Blog</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-solid fa-chevron-right text-success ms-2"></i> Contact</a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- widget -->
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 footer-widget">
+          <div class="fw-title  mt-3 mb-4">
+            <h3>Follow Us</h3>
+          </div>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-square-facebook text-success fs-4 my-2 ms-2"></i> Facebook</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-square-twitter text-success fs-4 my-2 ms-2"></i> Twitter</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-square-instagram text-success fs-4 my-2 ms-2"></i> Instagram</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-linkedin text-success fs-4 my-2 ms-2"></i> Linkedin</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><i class="fa-brands fa-square-pinterest text-success fs-4 my-2 ms-2"></i> Printerest</a></li>
+          </ul>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-3 footer-widget">
+          <a href="" target="_blank"><img src="img/home/logo.webp" class="foot my-5 rounded-5" width="214px"></a>
+        </div>
+      </div>
+    </div>
+    <!-- copyright -->
+    <div class="container">
+      <div class="d-flex flex-column flex-sm-row justify-content-center pt-4" style="border-top: 0.1px solid black;">
+        <p>© 2022 Company, Inc. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
